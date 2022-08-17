b9df6ab46f35e8ef42c5d0912a6d6090c0fd3727
WΔ <w@winterdelta.com>
2022-08-17 13:15:43 +0100

    Test API route; connect FE to BE.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App () {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(data => setData(data))
+  })
+
+  console.log(data)
+
   return (
     <div className='App'>
-      <div className='left-pane'></div>
+      <div className='left-pane'>{data}</div>
       <div className='right-pane'>Prometheus</div>
     </div>
   )
